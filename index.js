7e2cd11509d8b94d65b007522df04ab04482fa79
redebron20 <redebron20@gmail.com>
2021-06-28 21:31:43 -0400

    commit tree c575bc5e56044f0fe7fde8908ab8d1c19fdefc46

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+    static capitalize(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    
+    static sanitize(string) {
+      return string.replace(/[^A-Za-z0-9 '-]/g, '')
+    }
+
+    static titleize(sentence) {
+      let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+      let result = [];
+      let wordsArray = sentence.split(" ")
+      for (let i = 0; i < wordsArray.length; i++) {
+        if (i === 0) {
+          result.push(this.capitalize(wordsArray[i]))
+        } else {
+          if (exceptions.includes(wordsArray[i])){
+            result.push(wordsArray[i])
+          } else {
+            result.push(this.capitalize(wordsArray[i]))
+          }
+        }
+      }
+      return result.join(" ");
+    }
 }
+
